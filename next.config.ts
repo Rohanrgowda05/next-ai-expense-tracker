747f5154be94f13e438074e26af03fb761f49c05
Rohanrgowda05 <rohanrgowa05@gmail.com>
2025-09-29 13:18:52 +0530

    Completed record history component

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,11 @@
 import type { NextConfig } from "next";
+// next.config.js
+module.exports = {
+  devIndicators: {
+    appIsrStatus: false,   // disables the floating "N" symbol
+  },
+};
+
 
 const nextConfig: NextConfig = {
   /* config options here */
